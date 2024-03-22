01016ac76ad3f7bf66be4027ffd568558d2199e6
ei1906 <whitegorilla1906@gmail.com>
2024-03-22 12:51:19 +0900

    支払状況を管理するクラスを作成

--- lib/PaymentManager.dart
+++ lib/PaymentManager.dart
@@ -2,41 +2,41 @@ import 'package:tatekae/IndivisualPayment.dart';
 
 class PaymentManager {
   /* メンバ変数 */
-  int member_num;
+  int member_num = 0;
   Map<String, IndividualPayment> payment_status = {};
 
   /* constructor */
-  PaymentManager(this.member_num);
 
   /* getter */
-  int get_member_num() {
+  int getMemberNum() {
     return member_num;
   }
 
-  Map<String, IndividualPayment> get_all_payment_status() {
+  Map<String, IndividualPayment> getAllPaymentStatus() {
     return payment_status;
   }
 
-  IndividualPayment get_payment_status(String name) {
+  IndividualPayment getIndivisualPaymentStatus(String name) {
     if (!isRegistered(name)) {
       // nameが登録されていない
-      return new IndividualPayment('', 0, 0);
+      return IndividualPayment('', 0, 0);
     } else {
       return payment_status[name]!;
     }
   }
 
   /* setter */
-  void set_indivisual_payment(String name, int now_pay, int must_pay) {
-    payment_status[name] = new IndividualPayment(name, now_pay, must_pay);
+  void setIndivisualPayment(String name, int now_pay, int must_pay) {
+    payment_status[name] = IndividualPayment(name, now_pay, must_pay);
+    member_num++;
     return;
   }
 
-  void set_payment(
+  void setAllPayment(
       List<String> names, List<int> now_pays, List<int> must_pays) {
     int size = names.length;
     for (int i = 0; i < size; i++) {
-      set_indivisual_payment(names[i], now_pays[i], must_pays[i]);
+      setIndivisualPayment(names[i], now_pays[i], must_pays[i]);
     }
     return;
   }
@@ -45,10 +45,31 @@ class PaymentManager {
   bool isRegistered(String name) {
     return payment_status.containsKey(name);
   }
+
+  void printPaymentStatus() {
+    payment_status.forEach((key, value) {
+      value.printPayment();
+    });
+  }
+
+  void movePay(String from, String to, int payment) {
+    if (isRegistered(from) && isRegistered(to)) {
+      payment_status[from]!.addPayment(payment);
+      payment_status[to]!.subPayment(payment);
+    }
+    return;
+  }
 }
 
 void main() {
-  PaymentManager test = new PaymentManager(5);
-  test.set_indivisual_payment("taki", 0, 90);
-  print(test.get_member_num());
+  PaymentManager test = PaymentManager();
+  List<String> names = ["A", "B", "C", "D", "E"];
+  List<int> now_pays = [9000, 0, 0, 0, 0],
+      must_pays = [2000, 1500, 1500, 2500, 1500];
+  test.setAllPayment(names, now_pays, must_pays);
+  print(test.getMemberNum());
+  test.printPaymentStatus();
+  test.movePay("B", "A", 1500);
+  print("");
+  test.printPaymentStatus();
 }
