8772292349ce204e78f5c017ef095da671067fa1
ei1906 <whitegorilla1906@gmail.com>
2024-05-16 17:12:18 +0900

    プルダウンメニューを2つ設置

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tatekae/IndivisualPayment.dart';
 import 'package:tatekae/PaymentManager.dart';
 
 // 共有資源
@@ -198,13 +197,45 @@ class EntryScreen extends StatelessWidget {
 class SettlementScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // pmから全員分の名前を取得
+    List<String> name = pm.getMemberName();
+    // プルダウンメニューに初期値をセット
+    final ValueNotifier<String?> sender = ValueNotifier<String?>(name[0]);
+    final ValueNotifier<String?> reciever = ValueNotifier<String?>(name[0]);
+    // 表示するウィジェット
+    List<Widget> child = [];
+    child.add(getPullDownMenu(sender, name));
+    child.add(getPullDownMenu(reciever, name));
     return Scaffold(
       appBar: AppBar(
         title: Text('Settlement'),
       ),
       body: SingleChildScrollView(
-        child: Text("${pm.getIndivisualPaymentStatus('Person 1').now_payment}"),
-      ),
+          child: Column(
+        children: child,
+      )),
+    );
+  }
+
+  Widget getPullDownMenu(
+      ValueNotifier<String?> selectedItem, List<String> selectionList) {
+    return ValueListenableBuilder<String?>(
+      valueListenable: selectedItem,
+      builder: (context, value, child) {
+        return DropdownButton<String?>(
+          hint: Text('Select an item'),
+          value: value,
+          onChanged: (String? newValue) {
+            selectedItem.value = newValue;
+          },
+          items: selectionList.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        );
+      },
     );
   }
 }
